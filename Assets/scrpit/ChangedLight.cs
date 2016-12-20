23837e987ce099a9dcc05fef29bcd6080350d422
GuardLDW <303899225@qq.com>
2016-12-20 22:05:50 +0800

    1.创建了人物模型，通过按键能够控制人物左右旋转，向面朝方向移动 2.摄像机跟随人物移动 3.通过线程的方式控制光源颜色实现定时昼夜交替

---
+++ Assets/scrpit/ChangedLight.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+
+public class ChangedLight : MonoBehaviour {
+	
+	public Light light;
+
+	private string lightColor;
+
+	// Use this for initialization
+	void Start () {
+
+		lightColor = "yellow";
+		light.color = Color.yellow;
+
+		//白天与黑夜各持续24s
+		System.Timers.Timer t = new System.Timers.Timer(24000);
+		t.Elapsed += new System.Timers.ElapsedEventHandler(theout); //到达时间的时候执行事件
+		t.AutoReset = true;   //设置是执行一次（false）还是一直执行(true)；   
+		t.Enabled = true;     //是否执行System.Timers.Timer.Elapsed事件； 
+
+  
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (lightColor.Equals ("yellow")) {
+			
+			light.color = Color.yellow;
+			
+		} else if (lightColor.Equals ("black")) {
+			
+			light.color = Color.black;
+			
+		}
+
+	}
+
+	public void theout(object source, System.Timers.ElapsedEventArgs e)   
+	{   
+		changeLight();
+	}
+
+	//控件的颜色只能在主线程中修改，所以该子线程中改变颜色的标志状态
+	public void changeLight(){
+
+		if (lightColor.Equals ("yellow")) {
+
+			lightColor = "black";
+
+		} else if (lightColor.Equals ("black")) {
+
+			lightColor = "yellow";
+
+		}
+
+
+	}
+
+}
---
+++ Assets/scrpit/Player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//旋转
+		if (Input.GetKey(KeyCode.A)){
+
+			//逆时针（向左转）
+			this.transform.Rotate(Vector3.down, Space.World);
+
+		}else if(Input.GetKey(KeyCode.D)){
+			
+			this.transform.Rotate(Vector3.up, Space.World);
+			
+		}
+
+		//移动
+		if (Input.GetKey(KeyCode.W)){
+			
+			//将第二个参数由世界坐标改成自身坐标即可完成向自身面朝方向移动
+			this.transform.Translate(Vector3.forward / 25, Space.Self);
+			
+		}else if(Input.GetKey(KeyCode.S)){
+
+			this.transform.Translate(Vector3.back / 25, Space.Self);
+			
+		}
+
+
+
+	
+	}
+}
